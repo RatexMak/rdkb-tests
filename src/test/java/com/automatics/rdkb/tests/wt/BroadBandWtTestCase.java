a331b430e57cf68c4e95e315e581e0b7679f3e91
Athira Sivasankaran Nair <athira_sivasankarannair@comcast.com>
2023-01-11 12:51:26 -0500

    RPI Tests Refactor Reason for change: Tests refactoring for RPI Test Procedure: Not applicable. Risks: None
    Change-Id: I9118425ef408fa931242b3c0fe4bc63e4a49e7bc
    Signed-off-by: Athira Sivasankaran Nair <athira_sivasankarannair@comcast.com>

--- src/test/java/com/automatics/rdkb/tests/wt/BroadBandWtTestCase.java
+++ src/test/java/com/automatics/rdkb/tests/wt/BroadBandWtTestCase.java
@@ -25,216 +25,234 @@ import com.automatics.rdkb.wt.service.WtClientUtils;
 
 public class BroadBandWtTestCase extends AutomaticsTestBase {
 
-    /** URL for wt client */
-    String[] wtDetail = null;
-    boolean isWtClientPresent = false;
-    String wtSimulatorBaseUrl;
+	/** URL for wt client */
+	String[] wtDetail = null;
+	boolean isWtClientPresent = false;
+	String wtSimulatorBaseUrl;
 
-    @Test(enabled = true, dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dataProviderClass = AutomaticsTapApi.class, groups = BroadBandTestGroup.SYSTEM)
-    @TestDetails(testUID = "TC-RDKB-WTTEST-1000")
+	@Test(enabled = true, dataProvider = DataProviderConstants.PARALLEL_DATA_PROVIDER, dataProviderClass = AutomaticsTapApi.class, groups = BroadBandTestGroup.SYSTEM)
+	@TestDetails(testUID = "TC-RDKB-WTTEST-1000")
 
-    public void testVerifyWtClientConnection(Dut device) {
+	public void testVerifyWtClientConnection(Dut device) {
 
-	String testCaseId = "TC-RDKB-WTTEST-100";
-	String errorMessage = null;
+		String testCaseId = "TC-RDKB-WTTEST-100";
+		String errorMessage = null;
 
-	Map<String, String> extraProps;
+		Map<String, String> extraProps;
 
-	/** Test step **/
-	String step = "";
+		/** Test step **/
+		String step = "";
 
-	/** step number */
-	int stepNumber = 1;
-	boolean status = false;
+		/** step number */
+		int stepNumber = 1;
+		boolean status = false;
 
-	LOGGER.info("**********************************************************************************");
-	LOGGER.info(
-		"PRECONDITION 1: DESCRIPTION : Verify if the setup has candella clients and has got WISST services deployed and end point configured in CATS .");
-	LOGGER.info(
-		"PRECONDITION 1: ACTION :  Verify if the setup has candella clients and has got WISST services deployed and end point configured in CATS . ");
-	LOGGER.info(
-		"PRECONDITION 1: EXPECTED : Verify if the setup has candella clients and has got WISST services deployed and end point configured in CATS . ");
-	LOGGER.info("**********************************************************************************");
-
-	errorMessage = "Wt client URL is not configured or not in valid format. Skipping wt client execution";
-	try {
-	    // wtSimulatorBaseUrl - WT base url (fetch from device extra properties of
-	    // device)
-	    extraProps = ((Device) device).getExtraProperties();
-	    wtSimulatorBaseUrl = extraProps.get("wtSimulatorBaseUrl");
-	    // wtSimulatorBaseUrl = "http://localhost:8081/test";
-	    isWtClientPresent = CommonMethods.isNotNull(wtSimulatorBaseUrl);
-	    LOGGER.info("wtSimulatorBaseUrl is " + wtSimulatorBaseUrl);
-
-	} catch (Exception e) {
-	    LOGGER.error("Exception occured while trying to get URL from account", e);
-	}
-
-	isWtClientPresent = true;
-
-	String[] webpaList = { BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_2_4_GHZ_PRIVATE_SSID,
-		BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_5_GHZ_PRIVATE_SSID,
-		BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_2GHZ_SECURITY_KEYPASSPHRASE,
-		BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_5GHZ_SECURITY_KEYPASSPHRASE };
-
-	List<Station> stations = new ArrayList<Station>();
-	BroadBandResultObject result = new BroadBandResultObject();
-	try {
-	    if (isWtClientPresent) {
-
-		stepNumber = 1;
-		step = "S" + stepNumber;
-		status = false;
-		errorMessage = "Attempt to initiate new candela client and connected with Broadband device has failed";
 		LOGGER.info("**********************************************************************************");
-		LOGGER.info("STEP " + stepNumber
-			+ ": DESCRIPTION : Create 2 new client station on candella clients, connect to wifi and validate connectivity ");
-		LOGGER.info("STEP " + stepNumber
-			+ " : ACTION : Create new client stations for each band with existing station as model "
-			+ "and connect those stations to it respective band");
 		LOGGER.info(
-			"STEP " + stepNumber + " : EXPECTED : The client stations should be connected successfully");
+				"PRECONDITION 1: DESCRIPTION : Verify if the setup has candella clients and has got WISST services deployed and end point configured in CATS .");
+		LOGGER.info(
+				"PRECONDITION 1: ACTION :  Verify if the setup has candella clients and has got WISST services deployed and end point configured in CATS . ");
+		LOGGER.info(
+				"PRECONDITION 1: EXPECTED : Verify if the setup has candella clients and has got WISST services deployed and end point configured in CATS . ");
 		LOGGER.info("**********************************************************************************");
 
-		Map<String, String> webpaValues = tapEnv.executeMultipleWebPaGetCommands(device, webpaList);
+		errorMessage = "Wt client URL is not configured or not in valid format. Skipping wt client execution";
+		try {
+			// wtSimulatorBaseUrl - WT base url (fetch from device extra properties of
+			// device)
+			extraProps = ((Device) device).getExtraProperties();
+			wtSimulatorBaseUrl = extraProps.get("wtSimulatorBaseUrl");
+			isWtClientPresent = CommonMethods.isNotNull(wtSimulatorBaseUrl);
+			LOGGER.info("wtSimulatorBaseUrl is " + wtSimulatorBaseUrl);
 
-		// base url to wt-service will be fetched from AutomaticsProps
-		String wtBaseUrl = AutomaticsPropertyUtility.getProperty("wt.service.base.url");
-
-		LOGGER.info("wtBaseUrl is " + wtBaseUrl);
-
-		// wtBaseUrl = "https://automatics-apps.rdkcentral.com/WTService";
-		// LOGGER.info("wtBaseUrl is - " + wtBaseUrl);
-
-		// create station
-
-		status = WtClientUtils.createNewStations(
-			webpaValues.get(BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_2_4_GHZ_PRIVATE_SSID),
-			webpaValues.get(
-				BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_2GHZ_SECURITY_KEYPASSPHRASE),
-			wtBaseUrl, WiFiFrequencyBand.WIFI_BAND_2_GHZ, 1, wtSimulatorBaseUrl);
-
-		tapEnv.waitTill(BroadBandTestConstants.TWO_MINUTE_IN_MILLIS);
-
-		status = WtClientUtils.createNewStations(
-			webpaValues.get(BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_5_GHZ_PRIVATE_SSID),
-			webpaValues.get(
-				BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_5GHZ_SECURITY_KEYPASSPHRASE),
-			wtBaseUrl, WiFiFrequencyBand.WIFI_BAND_5_GHZ, 1, wtSimulatorBaseUrl);
-
-		tapEnv.waitTill(BroadBandTestConstants.TWO_MINUTE_IN_MILLIS);
-
-		LOGGER.info("status :  " + status);
-
-		//////////////////////////////////
-
-		// Trying to get Station details before creating a Station
-		stations = WtClientUtils.getAllStationsDetails(wtSimulatorBaseUrl, wtBaseUrl);
-
-		for (Station station : stations) {
-		    LOGGER.info("************************");
-
-		    LOGGER.info("Station Alias: " + station.getAlias());
-		    LOGGER.info("Station MAC: " + station.getMac());
-		    LOGGER.info("Station SSID: " + station.getSsid());
-		    LOGGER.info("Station Port: " + station.getPort());
-		    LOGGER.info("Station Mode: " + station.getMode());
-		    LOGGER.info("Station Status: " + station.getStatus());
-		    LOGGER.info("Station Down: " + station.getDownStatus());
-		    LOGGER.info("Station parentDev: " + station.getParentDev());
-		    LOGGER.info("Station ethernetInterface: " + station.getEthernetInterface());
-
-		    LOGGER.info("************************");
+		} catch (Exception e) {
+			LOGGER.error("Exception occured while trying to get URL from account", e);
 		}
 
-		// create station
-		status = WtClientUtils.createNewStations(
-			webpaValues.get(BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_2_4_GHZ_PRIVATE_SSID),
-			webpaValues.get(
-				BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_2GHZ_SECURITY_KEYPASSPHRASE),
-			wtBaseUrl, WiFiFrequencyBand.WIFI_BAND_2_GHZ, 1, wtSimulatorBaseUrl);
+		isWtClientPresent = true;
 
-		tapEnv.waitTill(BroadBandTestConstants.TWO_MINUTE_IN_MILLIS);
+		String[] webpaList = { BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_2_4_GHZ_PRIVATE_SSID,
+				BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_5_GHZ_PRIVATE_SSID,
+				BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_2GHZ_SECURITY_KEYPASSPHRASE,
+				BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_5GHZ_SECURITY_KEYPASSPHRASE };
 
-		status = WtClientUtils.createNewStations(
-			webpaValues.get(BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_5_GHZ_PRIVATE_SSID),
-			webpaValues.get(
-				BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_5GHZ_SECURITY_KEYPASSPHRASE),
-			wtBaseUrl, WiFiFrequencyBand.WIFI_BAND_5_GHZ, 1, wtSimulatorBaseUrl);
+		List<Station> stations = new ArrayList<Station>();
+		BroadBandResultObject result = new BroadBandResultObject();
+		try {
+			if (isWtClientPresent) {
 
-		tapEnv.waitTill(BroadBandTestConstants.TWO_MINUTE_IN_MILLIS);
+				Map<String, String> webpaValues = tapEnv.executeMultipleWebPaGetCommands(device, webpaList);
 
-		stations = WtClientUtils.getAllStationsDetails(wtSimulatorBaseUrl, wtBaseUrl);
+				// base url to wt-service will be fetched from AutomaticsProps
+				String wtBaseUrl = AutomaticsPropertyUtility.getProperty("wt.service.base.url");
 
-		for (Station station : stations) {
-		    LOGGER.info("************************");
+				LOGGER.info("wtBaseUrl is " + wtBaseUrl);
 
-		    LOGGER.info("Station Alias: " + station.getAlias());
-		    LOGGER.info("Station MAC: " + station.getMac());
-		    LOGGER.info("Station SSID: " + station.getSsid());
-		    LOGGER.info("Station Port: " + station.getPort());
-		    LOGGER.info("Station Mode: " + station.getMode());
-		    LOGGER.info("Station Status: " + station.getStatus());
-		    LOGGER.info("Station Down: " + station.getDownStatus());
-		    LOGGER.info("Station parentDev: " + station.getParentDev());
-		    LOGGER.info("Station ethernetInterface: " + station.getEthernetInterface());
+				LOGGER.info("**********************************************************************************");
+				LOGGER.info("PRECONDITION 2 : DESCRIPTION : Attempt to fetch Station details before creating Station ");
+				LOGGER.info("PRECONDITION 2 : ACTION : Fetch availbale stationdetails");
+				LOGGER.info("PRECONDITION 2 : EXPECTED : There should not be a client station");
+				LOGGER.info("**********************************************************************************");
 
-		    LOGGER.info("************************");
+				stations = WtClientUtils.getAllStationsDetails(wtSimulatorBaseUrl, wtBaseUrl);
+
+				for (Station station : stations) {
+					LOGGER.info("************************");
+
+					LOGGER.info("Station Alias: " + station.getAlias());
+					LOGGER.info("Station MAC: " + station.getMac());
+					LOGGER.info("Station SSID: " + station.getSsid());
+					LOGGER.info("Station Port: " + station.getPort());
+					LOGGER.info("Station Mode: " + station.getMode());
+					LOGGER.info("Station Status: " + station.getStatus());
+					LOGGER.info("Station Down: " + station.getDownStatus());
+					LOGGER.info("Station parentDev: " + station.getParentDev());
+					LOGGER.info("Station ethernetInterface: " + station.getEthernetInterface());
+
+					LOGGER.info("************************");
+				}
+
+				stepNumber = 1;
+				step = "S" + stepNumber;
+				status = false;
+				errorMessage = "Attempt to initiate new candela client and connected with Broadband device has failed";
+				LOGGER.info("**********************************************************************************");
+				LOGGER.info("STEP " + stepNumber
+						+ ": DESCRIPTION : Create 2 new client station on candella clients, connect to wifi and validate connectivity ");
+				LOGGER.info("STEP " + stepNumber
+						+ " : ACTION : Create new client stations for each band with existing station as model "
+						+ "and connect those stations to it respective band");
+				LOGGER.info(
+						"STEP " + stepNumber + " : EXPECTED : The client stations should be connected successfully");
+				LOGGER.info("**********************************************************************************");
+				// Create station
+				status = WtClientUtils.createNewStations(
+						webpaValues.get(BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_2_4_GHZ_PRIVATE_SSID),
+						webpaValues.get(
+								BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_2GHZ_SECURITY_KEYPASSPHRASE),
+						wtBaseUrl, WiFiFrequencyBand.WIFI_BAND_2_GHZ, 1, wtSimulatorBaseUrl);
+
+				tapEnv.waitTill(BroadBandTestConstants.TWO_MINUTE_IN_MILLIS);
+
+				status = WtClientUtils.createNewStations(
+						webpaValues.get(BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_5_GHZ_PRIVATE_SSID),
+						webpaValues.get(
+								BroadBandWebPaConstants.WEBPA_PARAM_DEVICE_WIFI_ACCESSPOINT_5GHZ_SECURITY_KEYPASSPHRASE),
+						wtBaseUrl, WiFiFrequencyBand.WIFI_BAND_5_GHZ, 1, wtSimulatorBaseUrl);
+
+				tapEnv.waitTill(BroadBandTestConstants.TWO_MINUTE_IN_MILLIS);
+				LOGGER.info("status :  " + status);
+
+				if (status) {
+					LOGGER.info("STEP " + stepNumber
+							+ " a : ACTUAL : Successfully created 2 new client station on candela client");
+
+					stations = WtClientUtils.getAllStationsDetails(wtSimulatorBaseUrl, wtBaseUrl);
+
+					for (Station station : stations) {
+						LOGGER.info("************************");
+
+						LOGGER.info("Station Alias: " + station.getAlias());
+						LOGGER.info("Station MAC: " + station.getMac());
+						LOGGER.info("Station SSID: " + station.getSsid());
+						LOGGER.info("Station Port: " + station.getPort());
+						LOGGER.info("Station Mode: " + station.getMode());
+						LOGGER.info("Station Status: " + station.getStatus());
+						LOGGER.info("Station Down: " + station.getDownStatus());
+						LOGGER.info("Station parentDev: " + station.getParentDev());
+						LOGGER.info("Station ethernetInterface: " + station.getEthernetInterface());
+
+						LOGGER.info("************************");
+					}
+
+					// Connect
+
+					tapEnv.waitTill(BroadBandTestConstants.ONE_MINUTE_IN_MILLIS);
+					LOGGER.info(" Going to check whether created stations are connected to gateway");
+					for (Station station : stations) {
+						result = WtClientUtils.verifyDevicesAreConnectedToGateway(device, tapEnv, station, true);
+						if (result.isStatus()) {
+							LOGGER.info("STEP " + stepNumber + " : ACTUAL :Station with MAC " + station.getMac()
+									+ " is connected to Gateway");
+						} else {
+							LOGGER.error("Station with MAC " + station.getMac() + " is  not connected to Gateway");
+							errorMessage = result.getErrorMessage();
+							LOGGER.error(result.getErrorMessage());
+						}
+					}
+
+				} else {
+					errorMessage = "Fail to create new client station on candela client";
+					LOGGER.error("STEP " + stepNumber + "  : ACTUAL : " + errorMessage);
+				}
+
+				tapEnv.updateExecutionStatus(device, testCaseId, step, status, errorMessage, true);
+
+				stepNumber = 2;
+				step = "S" + stepNumber;
+				status = false;
+				errorMessage = "Attempt to delete exsisting station failed";
+				LOGGER.info("**********************************************************************************");
+				LOGGER.info(
+						"STEP " + stepNumber + ": DESCRIPTION : Delete stations on candela that are created in step 1");
+				LOGGER.info("STEP " + stepNumber + " : ACTION : Delete all stations on candella client");
+				LOGGER.info("STEP " + stepNumber + " : EXPECTED : The client stations should be deleted successfully");
+				LOGGER.info("**********************************************************************************");
+
+				// delete station
+
+				for (Station station : stations) {
+
+					String deleteStatus = WtClientUtils.deleteStation(station.getAlias(), wtSimulatorBaseUrl,
+							wtBaseUrl);
+					status = CommonMethods.isNotNull(deleteStatus);
+					if (status) {
+
+						LOGGER.info("Station Alias: " + station.getAlias());
+						LOGGER.info("Station MAC: " + station.getMac());
+						LOGGER.info("Station SSID: " + station.getSsid());
+						LOGGER.info("Station Port: " + station.getPort());
+						LOGGER.info("Station Mode: " + station.getMode());
+						LOGGER.info("Station Status: " + station.getStatus());
+						LOGGER.info("Station Down: " + station.getDownStatus());
+						LOGGER.info("Station parentDev: " + station.getParentDev());
+						LOGGER.info("Station ethernetInterface: " + station.getEthernetInterface());
+
+						LOGGER.info("STEP " + stepNumber + " : ACTUAL : Successfully deleted Station with ID :"
+								+ station.getAlias());
+
+					} else {
+						LOGGER.error("STEP " + stepNumber + " : ACTUAL : Failed to delete Station with ID :"
+								+ station.getAlias());
+					}
+				}
+				tapEnv.updateExecutionStatus(device, testCaseId, step, status, errorMessage, true);
+
+				LOGGER.info("Trying to get Station details after deleting Stations");
+				stations = WtClientUtils.getAllStationsDetails(wtSimulatorBaseUrl, wtBaseUrl);
+
+				for (Station station : stations) {
+					LOGGER.info("************************");
+
+					LOGGER.info("Station Alias: " + station.getAlias());
+					LOGGER.info("Station MAC: " + station.getMac());
+					LOGGER.info("Station SSID: " + station.getSsid());
+					LOGGER.info("Station Port: " + station.getPort());
+					LOGGER.info("Station Mode: " + station.getMode());
+					LOGGER.info("Station Status: " + station.getStatus());
+					LOGGER.info("Station Down: " + station.getDownStatus());
+					LOGGER.info("Station parentDev: " + station.getParentDev());
+					LOGGER.info("Station ethernetInterface: " + station.getEthernetInterface());
+
+					LOGGER.info("************************");
+				}
+
+			} else {
+				LOGGER.error("No WT clients are available to validate..");
+			}
+		} catch (Exception e) {
+			LOGGER.info("Exception occured while trying to connect candela client", e);
 		}
-
-		// Connect
-
-		tapEnv.waitTill(BroadBandTestConstants.ONE_MINUTE_IN_MILLIS);
-		LOGGER.info(" Going to check whether created stations are connected to gateway");
-		for (Station station : stations) {
-		    result = WtClientUtils.verifyDevicesAreConnectedToGateway(device, tapEnv, station, true);
-		    if (result.isStatus()) {
-			LOGGER.info("Station with MAC " + station.getMac() + " is connected to Gateway");
-		    } else {
-			LOGGER.error("Station with MAC " + station.getMac() + " is  not connected to Gateway");
-			LOGGER.error(result.getErrorMessage());
-		    }
-		}
-
-		// delete station
-
-		for (Station station : stations) {
-
-		    String deleteStatus = WtClientUtils.deleteStation(station.getAlias(), wtSimulatorBaseUrl,
-			    wtBaseUrl);
-		    status = CommonMethods.isNotNull(deleteStatus);
-		    if (status) {
-			LOGGER.info("Successfully deleted Station with ID :" + station.getAlias());
-		    } else {
-			LOGGER.error("Failed to delete Station with ID :" + station.getAlias());
-		    }
-		}
-
-		// Trying to get Station details after deleting Stations
-		stations = WtClientUtils.getAllStationsDetails(wtSimulatorBaseUrl, wtBaseUrl);
-
-		for (Station station : stations) {
-		    LOGGER.info("************************");
-
-		    LOGGER.info("Station Alias: " + station.getAlias());
-		    LOGGER.info("Station MAC: " + station.getMac());
-		    LOGGER.info("Station SSID: " + station.getSsid());
-		    LOGGER.info("Station Port: " + station.getPort());
-		    LOGGER.info("Station Mode: " + station.getMode());
-		    LOGGER.info("Station Status: " + station.getStatus());
-		    LOGGER.info("Station Down: " + station.getDownStatus());
-		    LOGGER.info("Station parentDev: " + station.getParentDev());
-		    LOGGER.info("Station ethernetInterface: " + station.getEthernetInterface());
-
-		    LOGGER.info("************************");
-		}
-
-	    } else {
-		LOGGER.error("No WT clients are available to validate..");
-	    }
-	} catch (Exception e) {
-	    LOGGER.info("Exception occured while trying to connect candela client", e);
 	}
-    }
 
 }
